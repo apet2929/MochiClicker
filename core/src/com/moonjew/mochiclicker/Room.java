0aee42edc50b96740233d090874cc60ddeb2f108
apet2929 <56840582+apet2929@users.noreply.github.com>
2021-05-28 07:10:14 -0500

    Room system, still need Room handler

--- core/src/com/moonjew/mochiclicker/Room.java
+++ core/src/com/moonjew/mochiclicker/Room.java
@@ -1,15 +1,35 @@
 package com.moonjew.mochiclicker;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.moonjew.mochiclicker.entities.Cat;
 
 import java.util.HashMap;
 
-public abstract class Room {
-    HashMap<String, Integer> objects;
+public class Room {
+    Cat cat;
+    Rectangle rectangle;
+    Color color;
+    boolean focused;
 
-    public void render(SpriteBatch batch){
-        //render all objects here
+    public Room(Color color) {
+        this.rectangle = new Rectangle(20,20,MochiClicker.WIDTH-40, MochiClicker.HEIGHT-40);
+        this.cat = new Cat(new Texture("testcat.jpg"), 0,0, 100, 100, rectangle);
+        this.color = color;
     }
 
-
+    public void update(float deltaTime){
+        cat.update(deltaTime);
+    }
+    public Color getColor(){
+        return color;
+    }
+    public Rectangle getRectangle(){
+        return rectangle;
+    }
+    public Cat getCat(){
+        return cat;
+    }
 }
