935ca157371b30c83d2c18f18508faf03c1f18e1
apet2929 <56840582+apet2929@users.noreply.github.com>
2021-07-10 22:20:46 -0500

    Meters

---
+++ core/src/com/moonjew/mochiclicker/io/HappyMeter.java
@@ -0,0 +1,27 @@
+package com.moonjew.mochiclicker.io;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+
+public class HappyMeter {
+    Rectangle textureRect;
+    Texture sourceTexture;
+    TextureRegion[] textures;
+    public HappyMeter(Texture sourceTexture, Rectangle textureRect){
+        this.textureRect = textureRect;
+        this.sourceTexture = sourceTexture;
+        this.textures = new TextureRegion[5];
+        int width = sourceTexture.getWidth()/5;
+        int height = sourceTexture.getHeight();
+        for(int i = 0; i < 5; i++){
+            this.textures[i] = new TextureRegion(sourceTexture, width*i, 0, width, height);
+        }
+    }
+
+    public void render(SpriteBatch sb, int index){
+        sb.draw(textures[index], textureRect.x, textureRect.y, textureRect.width, textureRect.height);
+    }
+
+}
--- core/src/com/moonjew/mochiclicker/io/Meter.java
+++ core/src/com/moonjew/mochiclicker/io/Meter.java
@@ -2,6 +2,8 @@ package com.moonjew.mochiclicker.io;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 
 
@@ -9,10 +11,19 @@ public class Meter {
     Texture texture;
     Color color;
     Rectangle fillRect;
+    Rectangle textureRect;
+    public int fillHeight;
 
-    public Meter(Texture texture, Color color, Rectangle fillRect) {
+    public Meter(Texture texture, Color color, Rectangle textureRect, Rectangle fillRect) {
         this.texture = texture;
         this.color = color;
         this.fillRect = fillRect;
+        this.textureRect = textureRect;
+    }
+
+    public void render(SpriteBatch sb, ShapeRenderer sr){
+        sr.setColor(color);
+        sr.rect(fillRect.x, fillRect.y, fillRect.width, fillHeight);
+        sb.draw(texture, textureRect.x, textureRect.y, textureRect.width, textureRect.height);
     }
 }
